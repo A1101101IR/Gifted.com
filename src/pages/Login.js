9375070ffa68076544e7d50206496f409ccc2d4e
RainergrenOscar <oscar.rainergren@hotmail.com>
2022-01-13 17:28:30 +0100

    created login and signup page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,36 @@
-import React from "react";
+import "./Login.scss"
+import { Link } from "react-router-dom";
+
 
 const Login = () => {
   return (
-    <div>
-      <h1>login</h1>
-    </div>
+    <section className="section-login">
+      <div className="container">
+        <div className="split">
+          <div className="logo">
+            <span>Gifted.</span>
+          </div>
+          <div className="card" style={{height: "27rem"}} > 
+            <div className="card-text">
+              <span>Login</span>
+              <p>Don't have an account yet?<Link to="/pages/singup" className="link">Sign up</Link></p>
+            </div>
+            <div className="inputs">
+              <input type="text" placeholder="Username" required/>
+              <input type="text" placeholder="Password" required/>
+            </div>
+            <div className="button-container">
+              <button>Sign in</button>
+            </div>
+            <div className="bottom">
+              <input type="checkbox" className="check"/>
+           <span>I have read and agree to the <Link to="/pages/singup"className="link">Terms of Service</Link></span>
+          </div>
+          </div>
+        </div>
+      </div>
+    </section>
+   
   );
 };
 
