4fdfdc3f5b5311478f5615389fc74263be20b4f6
hellodit33 <pradetelodie@gmail.com>
2022-01-14 17:01:04 +0100

    fixed main.js and products.js

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,8 +1,20 @@
+import React from "react";
+import Logo from "../img/gifted.png";
+
 const Main = () => {
   return (
-    <div className="main">
-      <h1>Main</h1>
-    </div>
+    <div className="main main-columns">
+      <div className="main-rows">
+        <h1 className="main-element">Welcome to the world of Gift Cards</h1>
+        <p className="main-element">Here you will find all your favourite experiences giftcards to offer to your friends and family for Christmas, birthdays and life's successes.</p>
+        <button>Explore</button>
+      </div>
+
+      <div className="main-rows">
+        <img className="main-element" src={Logo} width={400}></img>
+      </div>
+
+    </div >
   );
 };
 
--- src/pages/products.js
+++ src/pages/products.js
@@ -1,5 +1,21 @@
+import React from "react";
+import Logo from "../img/gifted.png";
+
 const Products = () => {
-  return <h1>Products</h1>;
+  return (
+    <div className="main shop">
+      <h1 className="page-title">Products</h1>
+      <div className="main-products">
+        <div className="product">A treehotel night</div>
+        <div className="product">A night at the museum</div>
+        <div className="product">A cooking lesson</div>
+        <div className="product">A massage at the SPA</div>
+        <div className="product">A night at the opera</div>
+        <div className="product">A football game</div>
+      </div>
+
+    </div>
+  );
 };
 
 export default Products;
