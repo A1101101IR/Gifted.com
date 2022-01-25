23e2dee19cc5106108c06d915de7db18b9b102b4
RainergrenOscar <oscar.rainergren@hotmail.com>
2022-01-25 16:39:34 +0100

    added

--- src/pages/products.js
+++ src/pages/products.js
@@ -7,22 +7,7 @@ import SearchBar from "./SearchBar.js";
 const Products = () => {
   return (
     <div className="main-content">
-      {/* <div className="main shop">
-        <h1 className="page-title">Products</h1>
-      </div> */}
-
-      <div>
-        <div className="main-products">
-          <div className="product">A treehotel night</div>
-          <div className="product">A night at the museum</div>
-          <div className="product">A cooking lesson</div>
-          <div className="product">A massage at the SPA</div>
-          <div className="product">A night at the opera</div>
-          <div className="product">A football game</div>
-        </div>
-
-        <ProductData />
-      </div>
+      <ProductData />
     </div>
   );
 };
