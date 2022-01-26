1bac0e5e4dcb3d4332d18cdb8c04aa6679222dbc
hellodit33 <pradetelodie@gmail.com>
2022-01-26 11:00:52 +0100

    error away

--- src/components/product/ProductList.js
+++ src/components/product/ProductList.js
@@ -7,14 +7,6 @@ const ProductList = ({ product }) => {
         <div className={`products-card-sm ${product.company}`} key={product.id}>
           <div className="product-info-before">
             <h2>{product.titel}</h2>
-<<<<<<< HEAD
-            <p>{product.description}</p>
-            <span>{product.price} ;-kr</span>
-            <br />
-            <button>Details</button>
-            <button>buy</button>
-          </Link>
-=======
             <p>{product.description.substring(0, 125)}...</p>
           </div>
           <div className="product-pris-btn">
@@ -23,7 +15,6 @@ const ProductList = ({ product }) => {
               <button className="my-btn">Detalis</button>
             </Link>
           </div>
->>>>>>> 54721da2be6316f2c72da1d53138d1a1b667c86a
         </div>
       ))}
     </div>
