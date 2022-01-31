8d5e42fe98756ada6202e20240b7dd83083d2120
hellodit33 <pradetelodie@gmail.com>
2022-01-31 10:31:04 +0100

    small changes 4

--- src/components/product/product.js
+++ src/components/product/product.js
@@ -92,7 +92,7 @@ const Product = () => {
                 placeholder="Sven@svensson.se"
                 onChange={(e) => setReceiverMail(e.target.value)}
               />
-              <label>Vaule: </label>
+              <label>Value: </label>
               <input
                 type="number"
                 required
